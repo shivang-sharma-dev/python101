b3c23ba36e4bf2b99af9f1f9886294f457d2eae7
Shivang Sharma <shivangsharma@Shivangs-MacBook-Air.local>
2026-07-09 18:45:17 +0530

    basiccodes

---
+++ pythonbasics.py
@@ -0,0 +1,25 @@
+print(3.16*7)
+
+#Boolean To check wheather a value is True of False 
+
+print(3==3)
+print(3==4)
+
+print(True==1)
+print(False==0)
+print(True==0)
+
+result=4/2
+print(result==2)
+
+#String Text enclosed in single or double quotes
+print("Hello World")
+
+#We can Join the two strings using the + operator
+print("Hello"+"World")
+print('Hello World')
+
+
+#We can also use escape characters to include special characters in strings
+print("hello\nworld")
+print("DevOps\nis\nawesome")
