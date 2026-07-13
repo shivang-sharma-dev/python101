00694b6797cd640ee1f5c808c6b28e7dbc137d30
Shivang Sharma <shivangsharma@Shivangs-MacBook-Air.local>
2026-07-13 18:44:41 +0530

    30dayspython

---
+++ variableinpython.py
@@ -0,0 +1,32 @@
+#Concatenation of strings with the help of variables which can be used multiple times
+
+greeting="Good Morning"
+
+print(greeting+ " Guys ")
+
+#variables can be used with any datatypes and can be accessed too 
+
+float=2.4
+
+print(float)
+
+print(type(float))
+
+boolean=True
+
+print(boolean)
+
+print(type(boolean))
+
+var=2
+
+print(var)
+
+var=5
+
+print(var)
+
+#Naming Convention for python variable cannot be started using the special chars only alpha numeric are allowed . 
+#Python is case senstive 
+#python var names doesn't start with numbers . 
+
